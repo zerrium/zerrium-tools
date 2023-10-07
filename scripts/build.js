b769e737afa7fe547c003855a416b2d55adc2763
zerrium <zerrium@mail.com>
2023-10-07 23:06:09 +0700

    Fixed polyfill webpack 5 node configs

---
+++ scripts/build.js
@@ -0,0 +1,31 @@
+const rewire = require('rewire');
+const webpack = require('webpack');
+const defaults = rewire('react-scripts/scripts/build.js');
+
+const config = defaults.__get__('config');
+
+//Customize the webpack configuration here.
+config.resolve.fallback = {
+  ...config.resolve.fallback,
+  crypto: require.resolve("crypto-browserify"),
+  buffer: require.resolve('buffer'),
+  process: require.resolve('process/browser'),
+  stream: require.resolve('stream-browserify'),
+};
+
+config.module.rules = [
+  ...config.module.rules,
+  {
+    resolve: {
+      fullySpecified: false
+    }
+  }
+]
+
+config.plugins = [
+  ...config.plugins,
+  new webpack.ProvidePlugin({
+    Buffer: ['buffer', 'Buffer'],
+    process: 'process/browser',
+  }),
+];
---
+++ scripts/start.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/start.js');
+const webpackConfig = require('react-scripts/config/webpack.config');
+
+// Override to /node_modules/react-scripts/config/webpack.config.js
+defaults.__set__('configFactory', (webpackEnv) => {
+  let config = webpackConfig(webpackEnv);
+
+  //Customize the webpack configuration here.
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve("crypto-browserify"),
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
+    stream: require.resolve('stream-browserify'),
+  };
+
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      resolve: {
+        fullySpecified: false
+      }
+    }
+  ]
+
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+  ];
+
+  return config;
+});
