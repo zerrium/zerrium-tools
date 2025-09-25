709e71f4bd2092d45706d8f2e282938c125f5b9f
Willy Susilo <WS88199X@cimbniaga.co.id>
2025-09-25 11:09:17 +0700

    Fixed Character Counter

--- src/pages/Text Tools/CharacterCounter.tsx
+++ src/pages/Text Tools/CharacterCounter.tsx
@@ -5,43 +5,43 @@ const CharacterCounter = () => {
   const [count, setCount] = useState(0);
   return (
     <Flex
-          minH={'100vh'}
-          align={'center'}
-          justify={'center'}>
-            <Stack
-              spacing={4}
-              w={'full'}
-              maxW={'md'}
-              bg={useColorModeValue('white', 'gray.700')}
-              rounded={'lg'}
-              boxShadow={'lg'}
-              borderWidth={1}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}
-              p={6}>
-              <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-                Character Counter
-              </Heading>
-               
-                <Textarea
-                  placeholder="Input Text"
-                  _placeholder={{ color: 'gray.500' }}
-                  outlineColor={useColorModeValue('gray.300', 'gray.600')}
-                  value={text}
-                  onChange={
-                    (e) => {
-                      setText(e.target.value);
-                      setCount(e.target.value.length);
-                    }
-                  }
-                  fontFamily="monospace"
-                />
-                <Badge>
-                  Character Count: {count}
-                </Badge>
-              </Stack>
-              
-        </Flex>
-
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}>
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'2xl'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'lg'}
+          boxShadow={'lg'}
+          borderWidth={1}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          p={6}>
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+            Character Counter
+          </Heading>
+            <FormControl id="characterCounter">
+            <Textarea
+              placeholder="Input Text"
+              _placeholder={{ color: 'gray.500' }}
+              value={text}
+              onChange={
+                (e) => {
+                  setText(e.target.value);
+                  setCount(e.target.value.length);
+                }
+              }
+              fontFamily="monospace"
+              rows={10}
+              mb={2}
+            />
+            <Badge>
+              Character Count: {count}
+            </Badge>
+            </FormControl>
+        </Stack>
+    </Flex>
   )
 }
 
