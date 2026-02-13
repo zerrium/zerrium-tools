656bef604b55f2ba4c519b58b603bdc3b51681ca
zerrium <zerrium@mail.com>
2026-02-13 23:48:44 +0700

    Improved search functionality and added keyword for searching

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -40,7 +40,7 @@ interface LinkItemProps {
   name: string
   key: string
   icon?: IconType
-  child?: { name: string, link: string, element: ReactElement }[]
+  child?: { name: string, link: string, element: ReactElement, keyword?: string }[]
   link?: string
   element?: ReactElement
 }
@@ -50,45 +50,45 @@ export const LinkItems: Array<LinkItemProps> = [
   {
     name: 'Security Tools', icon: FiLock, key: "securityTools",
     child: [
-      { name: "Hash Calculator", link: "hash-calculator", element: <HashCalculator /> },
+      { name: "Hash Calculator", link: "hash-calculator", element: <HashCalculator />, keyword: "sha,sha3" },
       { name: "UUID Generator", link: "uuid-generator", element: <UuidGenerator /> },
       { name: "UUID Validator", link: "uuid-validator", element: <UuidValidator /> },
-      { name: "Password Generator", link: "password-generator", element: <PasswordGenerator /> },
+      { name: "Password Generator", link: "password-generator", element: <PasswordGenerator />, keyword: "pwd,psswd,psswrd" },
     ]
   },
   {
     name: 'Encoding Tools', icon: FiCode, key: "encodingTools",
     child: [
-      { name: "HTML Encoder", link: "html-encoder", element: <HtmlEncoder /> },
-      { name: "URL Encoder", link: "url-encoder", element: <UrlEncoder /> },
-      { name: "Text Encoder", link: "text-encoder", element: <TextEncoder /> },
-      { name: "File Encoder", link: "file-encoder", element: <FileEncoder /> },
+      { name: "HTML Encoder", link: "html-encoder", element: <HtmlEncoder />, keyword: "entity,decoder" },
+      { name: "URL Encoder", link: "url-encoder", element: <UrlEncoder />, keyword: "decoder,link" },
+      { name: "Text Encoder", link: "text-encoder", element: <TextEncoder />, keyword: "decoder,base64,base64url,hex,dec,oct" },
+      { name: "File Encoder", link: "file-encoder", element: <FileEncoder />, keyword: "decoder,base64,base64url,hex,dec,oct" },
     ]
   },
   {
     name: 'Text Tools', icon: FiFileText, key: "textTools",
     child: [
-      {name: "Character Counter", link: "character-counter", element: <CharacterCounter /> },
-      { name: "Text Comparator", link: "text-comparator", element: <TextComparator /> },
+      {name: "Character Counter", link: "character-counter", element: <CharacterCounter />, keyword: "length,calculator" },
+      { name: "Text Comparator", link: "text-comparator", element: <TextComparator />, keyword: "compare,diff" },
       { name: "Regex Templates", link: "regex", element: <Regex /> },
-      { name: "Beautifier/Minifier", link: "beautifier-minifier", element: <Beautifier /> },
-      { name: "JSON Viewer", link: "json-viewer", element: <JsonViewer /> },
-      { name: "JSON Comparator", link: "json-comparator", element: <JsonComparator /> },
+      { name: "Beautifier/Minifier", link: "beautifier-minifier", element: <Beautifier />, keyword: "format,json,html,css,js,javascript,xml,sql" },
+      { name: "JSON Viewer", link: "json-viewer", element: <JsonViewer />, keyword: "validator" },
+      { name: "JSON Comparator", link: "json-comparator", element: <JsonComparator />, keyword: "diff" },
       { name: "YAML Validator", link: "yaml-validator", element: <YamlValidator /> },
-      { name: "YAML Comparator", link: "yaml-comparator", element: <YamlComparator /> },
+      { name: "YAML Comparator", link: "yaml-comparator", element: <YamlComparator />, keyword: "diff" },
     ]
   },
   {
     name: 'Date Time Tools', icon: FiClock, key: "dateTimeTools",
     child: [
-        { name: "Date Time Calculator", link: "date-time-calculator", element: <DateTimeCalculator /> },
-      { name: "Date Time Converter", link: "date-time-converter", element: <DateTimeConverter /> },
+        { name: "Date Time Calculator", link: "date-time-calculator", element: <DateTimeCalculator />, keyword: "unix,timestamp,delta,diff,add,subtract,duration,between" },
+      { name: "Date Time Converter", link: "date-time-converter", element: <DateTimeConverter />, keyword: "unix,timestamp,formatter" },
     ]
   },
   {
     name: 'Debugging Tools', icon: LuCodeXml, key: "encodingTools",
     child: [
-      { name: "Stack Trace Viewer", link: "stack-trace-viewer", element: <StackTraceViewer /> },
+      { name: "Stack Trace Viewer", link: "stack-trace-viewer", element: <StackTraceViewer />, keyword: "java,debug,format" },
     ]
   },
 ]
